0ca8408d65fd85ab9cd559c714437693ffb4a1a5
Georgi Arnaudov <just_be@abv.bg>
2019-12-30 19:16:50 +0200

    Adds watches models to the shop-page, collection-preview, collection-item components

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,56 +1,56 @@
 const SHOP_DATA = [
   {
     id: 1,
-    title: "Hats",
-    routeName: "hats",
+    title: "Chronograph",
+    routeName: "chronograph",
     items: [
       {
         id: 1,
-        name: "Brown Brim",
+        name: "Casio Edifice",
         imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
-        price: 25
+        price: 135
       },
       {
         id: 2,
-        name: "Blue Beanie",
+        name: "Calvin Klein City Watch",
         imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
-        price: 18
+        price: 115
       },
       {
         id: 3,
-        name: "Brown Cowboy",
+        name: "Casio G-Shock",
         imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
-        price: 35
+        price: 99
       },
       {
         id: 4,
-        name: "Grey Brim",
+        name: "Seiko Astron",
         imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
-        price: 25
+        price: 760
       },
       {
         id: 5,
-        name: "Green Beanie",
+        name: "Swatch Clovercheck",
         imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
-        price: 18
+        price: 180
       },
       {
         id: 6,
-        name: "Palm Tree Cap",
+        name: "Tommy Hilfiger Kyle",
         imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
-        price: 14
+        price: 340
       },
       {
         id: 7,
-        name: "Red Beanie",
+        name: "Fossil Minimalist",
         imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-        price: 18
+        price: 180
       },
       {
         id: 8,
-        name: "Wolf Cap",
+        name: "Gucci G-Timeless",
         imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
-        price: 14
+        price: 1600
       },
       {
         id: 9,
@@ -62,93 +62,93 @@ const SHOP_DATA = [
   },
   {
     id: 2,
-    title: "Sneakers",
-    routeName: "sneakers",
+    title: "Smart",
+    routeName: "smart",
     items: [
       {
         id: 10,
-        name: "Adidas NMD",
+        name: "Fitbit Versa",
         imageUrl: "https://i.ibb.co/0s3pdnc/adidas-nmd.png",
         price: 220
       },
       {
         id: 11,
-        name: "Adidas Yeezy",
+        name: "Garmin Vivoactive 3",
         imageUrl: "https://i.ibb.co/dJbG1cT/yeezy.png",
-        price: 280
+        price: 270
       },
       {
         id: 12,
-        name: "Black Converse",
+        name: "Huawei Watch GT 2",
         imageUrl: "https://i.ibb.co/bPmVXyP/black-converse.png",
-        price: 110
+        price: 360
       },
       {
         id: 13,
-        name: "Nike White AirForce",
+        name: "Samsung Active 2",
         imageUrl: "https://i.ibb.co/1RcFPk0/white-nike-high-tops.png",
-        price: 160
+        price: 330
       },
       {
         id: 14,
-        name: "Nike Red High Tops",
+        name: "Garmin Fenix 5 Plus",
         imageUrl: "https://i.ibb.co/QcvzydB/nikes-red.png",
-        price: 160
+        price: 700
       },
       {
         id: 15,
-        name: "Nike Brown High Tops",
+        name: "Apple Watch 5",
         imageUrl: "https://i.ibb.co/fMTV342/nike-brown.png",
-        price: 160
+        price: 999
       },
       {
         id: 16,
-        name: "Air Jordan Limited",
+        name: "Apple Watch 4",
         imageUrl: "https://i.ibb.co/w4k6Ws9/nike-funky.png",
-        price: 190
+        price: 699
       },
       {
         id: 17,
-        name: "Timberlands",
+        name: "Garmin Forerunner",
         imageUrl: "https://i.ibb.co/Mhh6wBg/timberlands.png",
-        price: 200
+        price: 225
       }
     ]
   },
   {
     id: 3,
-    title: "Jackets",
-    routeName: "jackets",
+    title: "Automatic",
+    routeName: "automatic",
     items: [
       {
         id: 18,
-        name: "Black Jean Shearling",
+        name: "Fossil Townsman",
         imageUrl: "https://i.ibb.co/XzcwL5s/black-shearling.png",
-        price: 125
+        price: 330
       },
       {
         id: 19,
-        name: "Blue Jean Jacket",
+        name: "Skagen Holst",
         imageUrl: "https://i.ibb.co/mJS6vz0/blue-jean-jacket.png",
-        price: 90
+        price: 450
       },
       {
         id: 20,
-        name: "Grey Jean Jacket",
+        name: "Armani Exchange ATLC",
         imageUrl: "https://i.ibb.co/N71k1ML/grey-jean-jacket.png",
-        price: 90
+        price: 245
       },
       {
         id: 21,
-        name: "Brown Shearling",
+        name: "Timberland Biddeford",
         imageUrl: "https://i.ibb.co/s96FpdP/brown-shearling.png",
-        price: 165
+        price: 550
       },
       {
         id: 22,
-        name: "Tan Trench",
+        name: "Guess Helios",
         imageUrl: "https://i.ibb.co/M6hHc3F/brown-trench.png",
-        price: 185
+        price: 360
       }
     ]
   },
@@ -159,45 +159,45 @@ const SHOP_DATA = [
     items: [
       {
         id: 23,
-        name: "Blue Tanktop",
+        name: "Michael Kors Parker",
         imageUrl: "https://i.ibb.co/7CQVJNm/blue-tank.png",
-        price: 25
+        price: 700
       },
       {
         id: 24,
-        name: "Floral Blouse",
+        name: "Michael Kors Sofie",
         imageUrl: "https://i.ibb.co/4W2DGKm/floral-blouse.png",
-        price: 20
+        price: 580
       },
       {
         id: 25,
-        name: "Floral Dress",
+        name: "Gucci Swiss Quarts",
         imageUrl: "https://i.ibb.co/KV18Ysr/floral-skirt.png",
-        price: 80
+        price: 1700
       },
       {
         id: 26,
-        name: "Red Dots Dress",
+        name: "Emporio Armani Gianni",
         imageUrl: "https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png",
-        price: 80
+        price: 800
       },
       {
         id: 27,
-        name: "Striped Sweater",
+        name: "BOSS Symphony",
         imageUrl: "https://i.ibb.co/KmSkMbH/striped-sweater.png",
-        price: 45
+        price: 780
       },
       {
         id: 28,
-        name: "Yellow Track Suit",
+        name: "Fossil Virginia",
         imageUrl: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
         price: 135
       },
       {
         id: 29,
-        name: "White Blouse",
+        name: "Guess Lola",
         imageUrl: "https://i.ibb.co/qBcrsJg/white-vest.png",
-        price: 20
+        price: 170
       }
     ]
   },
@@ -208,39 +208,39 @@ const SHOP_DATA = [
     items: [
       {
         id: 30,
-        name: "Camo Down Vest",
+        name: "Emporio Armani Ceramica",
         imageUrl: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
-        price: 325
+        price: 580
       },
       {
         id: 31,
-        name: "Floral T-shirt",
+        name: "Bvlgari Octo",
         imageUrl: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
-        price: 20
+        price: 440
       },
       {
         id: 32,
-        name: "Black & White Longsleeve",
+        name: "Bulova Curv",
         imageUrl: "https://i.ibb.co/55z32tw/long-sleeve.png",
-        price: 25
+        price: 450
       },
       {
         id: 33,
-        name: "Pink T-shirt",
+        name: "Gucci Sync",
         imageUrl: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
-        price: 25
+        price: 340
       },
       {
         id: 34,
-        name: "Jean Long Sleeve",
+        name: "Casio Full Black",
         imageUrl: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
-        price: 40
+        price: 150
       },
       {
         id: 35,
-        name: "Burgundy T-shirt",
+        name: "Omega Seamaster",
         imageUrl: "https://i.ibb.co/mh3VM1f/polka-dot-shirt.png",
-        price: 25
+        price: 8000
       }
     ]
   }
