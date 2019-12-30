dd54449a2a0032b26added9325d064b0e138eea5
Georgi Arnaudov <just_be@abv.bg>
2019-12-30 21:45:52 +0200

    Adds header component with links to home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import {BrowserRouter,Route, Switch } from 'react-router-dom'
-import ShopPage from './pages/shop/shop.component';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component.jsx';
-
-const ChronographPage = () => (
-  <div>
-    <h1>CHRONOGRAPH PAGE</h1>
-  </div>
-);
-
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-    </Switch>
-  </BrowserRouter>
-
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
