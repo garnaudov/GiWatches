60ca5e0d749f3029f73f31e1928ccea9da4c3553
Georgi Arnaudov <just_be@abv.bg>
2020-01-30 20:05:37 +0200

    Cart-item counter implemented

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,10 +8,10 @@ import "./cart-icon.styles.scss";
 
 //import ShopPage from "../../pages/shop/shop.component";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -19,4 +19,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
