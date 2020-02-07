e25136eae121d1842c7f54b6e947e7fb8ca8f81a
Georgi Arnaudov <just_be@abv.bg>
2020-02-07 12:23:23 +0200

    Updating Stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_WBqax2FWVzS9QlpJScO07iuL";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="GiWatches Company"
+      billingAddress
+      shippingAddress
+      description={`Your total is ${price} BGN`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
