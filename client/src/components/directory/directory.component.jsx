556cdbc0769d3c058e330584130b7496a1e23591
Georgi Arnaudov <just_be@abv.bg>
2020-06-11 01:11:34 +0300

    Adds tests for signIn, signOut, forms, buttons,  actions, reducers, card and directory components

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
-import MenuItem from "../menu-item/menu-item.component";
-import "./directory.styles.scss";
+import { createStructuredSelector } from "reselect";
+
+import { DirectoryMenuContainer } from "./directory.styles";
 
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
-import { createStructuredSelector } from "reselect";
+import MenuItem from "../menu-item/menu-item.component";
 
-const Directory = ({ sections }) => (
-  <div className="directory-menu">
+export const Directory = ({ sections }) => (
+  <DirectoryMenuContainer>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
-  </div>
+  </DirectoryMenuContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections
+  sections: selectDirectorySections,
 });
 
 export default connect(mapStateToProps)(Directory);
