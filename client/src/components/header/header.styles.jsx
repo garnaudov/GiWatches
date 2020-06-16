b268935c8cac7968e3b93c662fea7f930e6184fd
Georgi Arnaudov <just_be@abv.bg>
2020-06-16 21:26:52 +0300

    Minor LogoContainer styling changes

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -16,7 +16,7 @@ export const HeaderContainer = styled.div`
 
 export const LogoContainer = styled(Link)`
   height: 100%;
-  width: 70px;
+  width: 100px;
   padding: 25px;
   @media screen and (max-width: 800px) {
     width: 50px;
